21456d863b07c46c6e1755f68bf58dca6018b483
Asger Hautop Drewsen <asgerdrewsen@gmail.com>
2017-06-19 15:15:28 +0200

    Make auprint a local printer installer

--- auprint.py
+++ auprint.py
@@ -1,6 +1,34 @@
+from sys import exit
 import socket
 from subprocess import check_call, check_output, CalledProcessError, DEVNULL
 from getpass import getpass
+import configparser
+
+class Config(object):
+	def __init__(self, filename):
+		self._cp = configparser.ConfigParser()
+		self._filename = filename
+		self._cp.read(filename)
+
+	def __setattr__(self, key, value):
+		if key.startswith('_'):
+			super().__setattr__(key, value)
+			return
+
+		if value is None:
+			self._cp.remove_option(self._cp.default_section, key)
+		else:
+			self._cp.set(self._cp.default_section, key, value)
+
+		with open(self._filename, 'w') as f:
+			self._cp.write(f)
+
+	def __getattribute__(self, key):
+		if key.startswith('_'):
+			return super().__getattribute__(key)
+
+		return self._cp.get(self._cp.default_section, key, fallback=None)
+
 
 class AUAuthenticationError(BaseException):
 	pass
@@ -14,21 +42,20 @@ class AUPrint(object):
 	PPD = '/usr/share/ppd/cupsfilters/Generic-PDF_Printer-PDF.ppd'
 	DOMAIN = 'uni'
 	BUILDING_NAMES = {
-		'1530': 'Matematik',
+		'1530': 'matematik',
 
-		'5335': 'Nygaard',
-		'5340': 'Babbage',
-		'5341': 'Turing',
-		'5342': 'Ada',
-		'5343': 'Bush',
-		'5344': 'Benjamin',
-		'5345': 'Dreyer',
-		'5346': 'Hopper',
-		'5347': 'Wiener',
+		'5335': 'nygaard',
+		'5340': 'babbage',
+		'5341': 'turing',
+		'5342': 'ada',
+		'5343': 'bush',
+		'5344': 'benjamin',
+		'5345': 'dreyer',
+		'5346': 'hopper',
+		'5347': 'wiener',
 	}
+	BUILDING_NUMBERS = {v: k for k, v in BUILDING_NAMES.items()}
 
-	MANGLE_SUFFIX = '__auprint__'
-	
 	auid = None
 	password = None
 	printers = None
@@ -40,31 +67,18 @@ class AUPrint(object):
 		try:
 			self.printers = self.get_remote_printer_list()
 		except CalledProcessError:
+			raise
 			raise AUAuthenticationError()
-	
-	def mangle_name(self, name):
-		return MANGLE_SUFFIX + name
-
-	def demangle_name(self, name):
-		if name.startswith(MANGLE_SUFFIX):
-			return name[len(MANGLE_SUFFIX):]
-		else:
-			return None
-	
-	def is_auprint_printer(self, name):
-		return demangle_name(name) != None
 
 	def pretty_name(self, name):
 		parts = name.split('-')
 		if len(parts) == 1:
-			return None
-		
-		building = parts[0]
-		if building not in BUILDING_NAMES:
-			return None
+			return name
 
-		parts[0] = BUILDING_NAMES[parts[0]]
-		return '-'.join(parts)
+		building = self.BUILDING_NAMES.get(parts[0], parts[0])
+		number = parts[1]
+
+		return '%s-%s' % (building, number)
 
 	def get_remote_printer_list(self):
 		out = str(check_output(['smbclient', '-I', self.HOST, '-L', self.HOST, '-U',
@@ -95,39 +109,84 @@ class AUPrint(object):
 					continue
 
 				name = l.split()[1]
-				if self.is_auprint_printer(name):
-					printers.append(self.demangle_name(name))
+				printers.append(name)
 
 			return printers
 		except CalledProcessError:
 			return []
 
-	def install_printer(self, name):
+	def install_printer(self, name, install_name):
 		if name in self.printers:
-			check_call(['lpadmin', '-p', self.mangle(name), '-E', '-P', self.PPD, '-v',
+			check_call(['lpadmin', '-p', install_name, '-E', '-P', self.PPD, '-v',
 				        'smb://{}\\{}:{}@{}/{}'.format(self.DOMAIN, self.auid, self.password, self.IP, name)])
 		else:
 			raise PrinterNotFoundError()
 
 	def delete_printer(self, name):
 		if name in self.local_printer_names():
-			check_call(['lpadmin', '-x', self.mangle(name)])
+			check_call(['lpadmin', '-x', name])
 		else:
 			raise PrinterNotFoundError()
-	
-	def print(self, name, f):
+
+	def print(self, name,  f):
 		if name in self.local_printer_names():
-			check_call(['lpr', '-E', '-P', self.mangle(name), f])
+			check_call(['lpr', '-E', '-P', name, f])
 		else:
 			raise PrinterNotFoundError()
 
 if __name__ == '__main__':
-	auid = 'au522953'
-	password = getpass()
+	config = Config('config.ini')
 
-	P = AUPrint(auid, password)
-	p = '5335-394-c'
-	P.install_printer(p)
-	P.print(p, '/home/asger/Downloads/pdf-sample.pdf')
+	logged_in = False
+	while not logged_in:
+		while not config.auid:
+			config.auid = input('AUID: ').strip()
+			if not config.auid.startswith('au'):
+				config.auid = None
 
+		while not config.password:
+			config.password = getpass().strip()
 
+		try:
+			auprint = AUPrint(config.auid, config.password)
+			logged_in = True
+		except AUAuthenticationError:
+			print('Invalid auid/password combination')
+			config.auid = None
+			config.password = None
+
+	printers = auprint.get_remote_printer_list()
+
+	building = input('Building number/name: ')
+	building_number = AUPrint.BUILDING_NUMBERS.get(building, building)
+
+	matched_printers = [p for p in printers if p.startswith(building_number)]
+	if len(matched_printers) == 0:
+		print('No printers found')
+	else:
+		print('Available printers: ')
+		for i, p in enumerate(matched_printers):
+			print('(%s)\t%s' % (i + 1, p))
+
+		opt = input('Printer to install: ')
+		try:
+			opt = int(opt)
+		except ValueError:
+			exit()
+
+		opt -= 1
+		if not (0 <= opt < len(matched_printers)):
+			exit()
+
+		printer = matched_printers[opt]
+		name = auprint.pretty_name(printer)
+
+		print()
+		print('Selected', printer)
+		custom_name = input('Install name [%s]: ' % name)
+		if custom_name:
+			name = custom_name
+
+		auprint.install_printer(printer, name)
+
+		print('Successfully added printer %s as %s' % (printer, name))
